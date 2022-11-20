397c1879f08c9c43de0387f0c07ed76be99a18b5
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-11-20 14:37:06 +0100

    Change WeatherBox component to display diffrent views based on server status

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,31 +2,42 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
-const WeatherBox = (props) => {
-  let [finalData, setFinalData] = useState({});
+const WeatherBox = () => {
+  let [finalData, setFinalData] = useState('');
+  let [pending, setPending] = useState(false);
+  let [error, setError] = useState(false);
 
   const handleCityChange = useCallback((city) => {
+    setFinalData('');
+    setPending(true);
+    setError(false);
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b5b4985111af5d4a7d5fa8049a8a585f&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
-        setFinalData(weatherData);
-      });
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+
+          setFinalData(weatherData);
+        });
+      } else setError(true);
+    });
+    setPending(false);
   }, []);
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary data={finalData} />
-      <Loader />
+      {finalData && !pending && !error && <WeatherSummary data={finalData} />}
+      {pending && <Loader />}
+      {error && <ErrorBox />}
     </section>
   );
 };
